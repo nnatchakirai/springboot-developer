d830ee705a99df1a72ad1e6862091a8a40bef216
Minyoug Lee <ysunrs38w@gmail.com>
2026-09-14 10:39:40 +0900

    Project Setup for Test. HelloWordController를 생성함

--- src/main/java/me/mylee/springbootdeveloper/MemberController.java
+++ src/main/java/me/mylee/springbootdeveloper/MemberController.java
@@ -10,16 +10,9 @@ import java.util.List;
 
 @RestController
 public class MemberController {
-    private final EnableSpringDataWebSupport.SpringDataWebConfigurationImportSelector springDataWebConfigurationImportSelector;
-    private final ApplicationArguments springApplicationArguments;
-
     @Autowired
     private MemberService memberService;
 
-    public MemberController(EnableSpringDataWebSupport.SpringDataWebConfigurationImportSelector springDataWebConfigurationImportSelector, ApplicationArguments springApplicationArguments) {
-        this.springDataWebConfigurationImportSelector = springDataWebConfigurationImportSelector;
-        this.springApplicationArguments = springApplicationArguments;
-    }
 
     // 요청을 받아서 적절한 비즈니스 로직으로 연결
     // http://localhost:8080/member 요청과 메서드를 연결
